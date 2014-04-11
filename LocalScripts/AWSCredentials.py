2537921298a450f3243880db2dee45d7ae456d2c
jeyuan <jeyuan@eng.ucsd.edu>
2014-04-11 02:22:54 -0700

    Fixed secret_key exposure

--- LocalScripts/AWSCredentials.py
+++ LocalScripts/AWSCredentials.py
@@ -2,8 +2,8 @@
 # Change entries here to match your own #
 # Get values for the first two entries from the "Security crediential" Tab
 # Under in the menu under your name.
-aws_access_key_id='AKIAJE4OLTMRKE5SCDLQ'
-aws_secret_access_key='43ENEQTsqm3Ge4CWsCCNSIbBeZrgsrul/SVat88x'
+aws_access_key_id='TESTTRIAL'
+aws_secret_access_key='SECRET2'
 # Get the Keypair in the EC2 Dashboard page.
 keyPairFile="/home/Jeffrey/CSE291/CSE291_k2.pem" # name of file keeping local key
 key_name="CSE291" # name of keypair (not name of file where key is stored)
